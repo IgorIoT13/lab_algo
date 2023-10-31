d522e9d6a51f5ea2d659888be4941445e2e9507c
IgorIoT13 <ihor.slodziak.ir.2022@lpnu.ua>
2023-10-31 16:11:15 +0200

    lab4

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-from binar_tree import BinaryTree
+from model.map_reader import MapReader
 
-# Створюємо бінарне дерево
-root = BinaryTree(1)
-root.left = BinaryTree(3)
-root.right = BinaryTree(2)
-root.left.left = BinaryTree(7)
-root.left.right = BinaryTree(11)
-root.left.left.left = BinaryTree(8)
-root.left.left.left.left = BinaryTree(9)
+map = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 1, 0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1]
+]
+mapreader = MapReader(map)
 
-target_element = 9
-path_to_element_1 = root.find_path_to_element(root, target_element)
-target_element_2 = 2
-path_to_element_2 = root.find_path_to_element(root, target_element_2)
+mapreader.printer()
 
-
-def Fight(fir_arr, last_arr):
-    if fir_arr is None or last_arr is None:
-        return "Error"
-
-    min_len = len(fir_arr)
-    max_len = len(last_arr)
-
-    for i in fir_arr:
-        if i in last_arr:
-            min_len -= 1
-            max_len -= 1
-
-    result = abs(max_len + min_len)
-
-    if min_len <= 1 and min_len == max_len:
-        result -= 2
-
-    return result
+arr = mapreader.land_finder()
+print(len(arr))
 
 
-print(Fight(path_to_element_1, path_to_element_2))
+
